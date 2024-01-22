b262812bed131af6c05df5db64e066c86bc46f07
phú đặng <phubi268@gmail.com>
2024-01-22 13:24:05 +0700

    models

--- src/models/connect.js
+++ src/models/connect.js
@@ -1,12 +1,12 @@
-import { Sequelize } from 'sequelize'
-import dbConfig from '../configs/db.config.js'
+import { Sequelize } from "sequelize";
+import dbConfig from "../configs/db.config.js";
 
-let { dbHost, dbUser, dbPass, dbPort, dbDialect, dbName } = dbConfig
+let { dbHost, dbUser, dbPass, dbPort, dbDialect, dbName } = dbConfig;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
   dialect: dbDialect,
   port: dbPort,
-})
+});
 
-export default sequelize
+export default sequelize;
