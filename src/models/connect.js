2aa159c523120a7c000feea3f7735e88197f5813
Thành Phan <th.phan1408@gmail.com>
2024-01-22 10:02:12 +0700

    update dbConfig & cloudinaryConfig & setup dotenv

---
+++ src/models/connect.js
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize'
+import dbConfig from '../configs/db.config.js'
+
+let { dbHost, dbUser, dbPass, dbPort, dbDialect, dbName } = dbConfig
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+  host: dbHost,
+  dialect: dbDialect,
+  port: dbPort,
+})
+
+export default sequelize
