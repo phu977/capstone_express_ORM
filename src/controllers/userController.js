ecfb70974db1b7dd7070eef5c6669a13cbf96d30
Thành Phan <th.phan1408@gmail.com>
2024-01-24 09:59:46 +0700

    update for userRoutes and some things

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import sequelize from '../models/connect.js'
 import initModels from '../models/init-models.js'
+import bcrypt from 'bcrypt'
 
 const conn = initModels(sequelize)
 
@@ -12,8 +13,46 @@ const getUser = async (req, res) => {
     })
     res.status(200).send(data)
   } catch (error) {
-    res.status(500).send(error)
+    res.status(500).send({ message: error.message })
   }
 }
 
-export { getUser }
+const updateUser = async (req, res) => {
+  try {
+    const { userID } = req.params
+    let { email, mat_khau, ho_ten, tuoi, anh_dai_dien } = req.body
+
+    let user = await conn.nguoi_dung.findOne({
+      where: {
+        nguoi_dung_id: userID,
+      },
+    })
+
+    if (!user) {
+      res.status(404).send({ status: 404, message: 'User not found' })
+      return
+    }
+
+    let encodePassword = bcrypt.hashSync(mat_khau, 10)
+
+    let updateData = {
+      email,
+      mat_khau: encodePassword,
+      ho_ten,
+      tuoi,
+      anh_dai_dien: req.file ? req.file.path : user.anh_dai_dien,
+    }
+
+    await conn.nguoi_dung.update(updateData, {
+      where: {
+        nguoi_dung_id: userID,
+      },
+      data: updateData,
+    })
+    res.status(200).send({ status: 200, message: 'Update user successfully' })
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+}
+
+export { getUser, updateUser }
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -6,7 +6,9 @@ import {
   getListImage,
   searchImage,
   getInfoImageUser,
+  createImage,
 } from "../controllers/imageController.js";
+import uploadCloud from "../configs/cloudinary.config.js";
 
 const imageRoutes = express.Router();
 
@@ -16,5 +18,6 @@ imageRoutes.get("/info-image-user/:imageID", getInfoImageUser);
 imageRoutes.get("/get-list-image-save-by-user/:userID", getImageSaveByUser);
 imageRoutes.get("/get-list-image-create-by-user/:userID", getImageCreateByUser);
 imageRoutes.delete("/delete-image/:imageID", deleteImage);
+imageRoutes.post("/create-image",  uploadCloud.single('file'), createImage);
 
 export default imageRoutes;
